d4d188fbffd2e9db477f0736ec75bd6fd050ff3a
VerSionHwang <iot_cx@outlook.com>
2025-01-22 11:17:22 +0800

    perf:调整依赖文件中部分包的版本；update package version in requirements.txt

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from tasks import getTestDataTasks
 scheduler = BackgroundScheduler()
 
 # 在此处添加任务
-# 添加任务：定时同步打卡记录，每2小时执行一次
+# 添加任务：定时同步测试数据，每2小时执行一次
 scheduler.add_job(getTestDataTasks.get_test_data, 'cron', hour='*/2', id='get_test_data',
                   kwargs={'begin_date': '', 'end_date': ''})
 
